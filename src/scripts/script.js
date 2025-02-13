af9385e7c270da1d4d6ed85003625a21b1397a0a
Erik Gomes <gomeserik35@gmail.com>
2025-02-12 23:40:28 -0300

    deploy test 6.5

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,15 @@
-function toggleNavBar() {
-  const navBar = document.getElementById('headder');
-    navBar.style.width = "100px";
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("full-text");
+  var btnText = document.getElementById("read-more");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "block";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
