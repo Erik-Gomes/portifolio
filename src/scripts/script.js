188298a0bca4a25219fc1a50ecd4f1f543a7a309
Erik Gomes <gomeserik35@gmail.com>
2025-02-13 23:08:15 -0300

    deploy test 6.7

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -13,3 +13,33 @@ function readMore() {
     moreText.style.display = "inline";
   }
 }
+
+function incressPadding() {
+  var padding = document.getElementById("about-me");
+  padding.style.paddingTop = "3.3em";
+}
+
+function highlightNav() {
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll(".link");
+  let scrollY = window.scrollY;
+
+  sections.forEach((section, index) => {
+      let sectionTop = section.offsetTop - 70; // Ajuste para navbar fixa
+      let sectionHeight = section.clientHeight;
+
+      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+          // Remove a classe ativa de todos os links
+          navLinks.forEach(link => link.classList.remove("active"));
+          // Adiciona a classe ativa ao link correspondente
+          navLinks[index].classList.add("active");
+      }
+  });
+}
+
+// Detecta rolagem e aplica a função
+window.addEventListener("scroll", highlightNav);
+
+function toggleExpand(card) {
+  card.classList.toggle("expanded");
+}
